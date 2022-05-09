ba98e939c1c8b329651e1439780c9f94d105c9d9
AlexBlacksmith <unbloody.alex@gmail.com>
2022-05-09 21:56:10 +0500

    The basic file structure has been created. Homepage template ready

---
+++ app.mjs
@@ -0,0 +1,35 @@
+import express from 'express';
+const app = express();
+const HOST = 'localhost';
+const PORT = 3000;
+
+//Setting view engine
+app.set('view engine', 'ejs');
+
+//Public files
+app.use(express.static('public'));
+
+//index page route
+app.get('/', (req, res) => {
+	res.render('index.ejs');
+});
+
+//statistics page route
+app.get('/statistics', (req, res) => {
+	res.render('statistics.ejs');
+});
+
+//categories page route
+app.get('/categories', (req, res) => {
+	res.render('categories.ejs');
+});
+
+//guideline page route
+app.get('/guideline', (req, res) => {
+	res.render('guideline.ejs');
+});
+
+
+
+app.listen(PORT);
+console.log(`Server has started on the ${HOST}: ${PORT}`);
