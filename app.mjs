982d749df44edcfef14eae774a17bb92ca799b28
AlexBlacksmith <unbloody.alex@gmail.com>
2022-05-16 22:43:59 +0500

    Finished work on CRUD operations with records in local storage.

--- app.mjs
+++ app.mjs
@@ -14,19 +14,9 @@ app.get('/', (req, res) => {
 	res.render('index.ejs');
 });
 
-//statistics page route
-app.get('/statistics', (req, res) => {
-	res.render('statistics.ejs');
-});
-
 //categories page route
 app.get('/categories', (req, res) => {
 	res.render('categories.ejs');
-});
-
-//guideline page route
-app.get('/guideline', (req, res) => {
-	res.render('guideline.ejs');
 });
 
 //all-records page route
@@ -34,6 +24,9 @@ app.get('/all-records', (req, res) => {
 	res.render('all-records.ejs');
 });
 
+app.get('/create', (req, res) => {
+	res.render('create-record.ejs');
+});
 
 
 app.listen(PORT);
